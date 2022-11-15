29fbbc4c8b90cf5b3f689789e7f796b98c6101a9
gogutan <50437473+gogutan@users.noreply.github.com>
2022-11-15 23:54:56 +0900

    Fix bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -3,10 +3,10 @@ javascript: (
   function () {
     var deployedUrl = 'https://example.com';
     var word = window.getSelection().toString();
-    var replacedBody = document.body.innerHTML.replace(/<\/h[1-6]>/g, '.').replace(/<.*?>/g, '').replace(/\s{2,}/g, '.');
+    var replacedBody = document.body.innerHTML.replace(/<\/h[1-6]>/g, '.').replace(/<.*?>/g, ' ').replace(/\s{3,}/g, '.');
     var regexp = new RegExp('[^\\.]*' + word + '(.*?\\.)');
-    var sentence = replacedBody.match(regexp)[0].trim();
-    var title = document.title;
+    var sentence = replacedBody.match(regexp)[0].trim().replace(/"/g, '“');
+    var title = document.title.replace(/"/g, '“');
     var url = window.location.href;
     var fetchingUrl = `${deployedUrl}?word=${encodeURIComponent(word)}&sentence=${encodeURIComponent(sentence)}&title=${encodeURIComponent(title)}&url=${encodeURIComponent(url)}`;
     document.addEventListener("securitypolicyviolation", (e) => {
