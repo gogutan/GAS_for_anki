d6ba6f372ac00751cb3312ad670d0e9d2e2791c4
gogutan <50437473+gogutan@users.noreply.github.com>
2023-04-15 22:31:43 +0900

    Add bookmarklet_for_cnn10

--- ankiconnect.js
+++ ankiconnect.js
@@ -110,7 +110,7 @@ const main = async () => {
     await textToSpeech(process.env.AZUREKEY, 'eastus', word, `./medias/${word}.mp3`, voice)
     const wordAudio = await storeMediaFile(`_${word}.mp3`, `${__dirname}/medias/${word}.mp3`)
     await textToSpeech(process.env.AZUREKEY, 'eastus', sentence, `./medias/${word}Sentence.mp3`, voice)
-    const sentenceAudio = await storeMediaFile(`_${word}Sentence.mp3`, `${__dirname}/medias/${word}Sentence.mp3`)
+    const sentenceAudio = link.includes('youtube.com') ? '' : await storeMediaFile(`_${word}Sentence.mp3`, `${__dirname}/medias/${word}Sentence.mp3`)
     return [word, sentence, title, link, `[sound:${wordAudio}]`, `[sound:${sentenceAudio}]`]
   }))
 
---
+++ bookmarklet_for_cnn10.js
@@ -0,0 +1,18 @@
+// Copy and paste this script to the URL field of your bookmark.
+javascript: (
+  function () {
+    var deployedUrl = 'https://example.com';
+    var word = window.getSelection().toString();
+    var replacedBody = document.body.innerHTML.replace(/<\/h[1-6]>/g, '.').replace(/<.*?>/g, ' ').replace(/\s{3,}/g, ' ');
+    var regexp = new RegExp('[^\\.]*' + word + '(.*?\\.)');
+    var sentence = replacedBody.match(regexp)[0].trim().replace(/"/g, '“');
+    var title = document.title.replace(/"/g, '“');
+    var url = window.prompt().match(/(http.*?)(?=")/)[0];
+    var fetchingUrl = `${deployedUrl}?word=${encodeURIComponent(word)}&sentence=${encodeURIComponent(sentence)}&title=${encodeURIComponent(title)}&url=${encodeURIComponent(url)}`;
+    document.addEventListener("securitypolicyviolation", (e) => {
+      window.location = fetchingUrl;
+      setTimeout(() => { window.stop(); }, 200);
+    });
+    fetch(fetchingUrl);
+  }
+)()
